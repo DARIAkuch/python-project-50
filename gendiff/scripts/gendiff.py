2b63386a7c0291b8d6a2fc262fb94ce32822ac74
Daria <d.kuchkina2000@gmail.com>
2025-05-22 16:50:50 +0300

    formatters updated, tests updated

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,8 @@
 import argparse
 
+from gendiff.scripts.generate_diff import generate_diff
+
+
 def parser_function():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
@@ -13,8 +16,15 @@ def parser_function():
 
     return parser.parse_args()
 
+
 def main():
-    parser_function()
+    args = parser_function()
+
+    file_path1 = args.first_file
+    file_path2 = args.second_file
+
+    result = generate_diff(file_path1, file_path2, formatter=args.format)
+    print(result)
 
 
 if __name__ == "__main__":
